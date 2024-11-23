9828e9a7197c6139ee7d48846c1dd4537d2bcfd3
Koshevoy Pavel <94282297+chromasomaprime@users.noreply.github.com>
2024-11-23 03:33:42 +0200

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include "func.hpp"
 
-void Read(int mas[N_Max][M_Max], int& n, int& m)
- {
-    std::cout << "enter n ";
+void readMatrix(int matrix[N_Max][N_Max], int& n)
+{
+    std::cout << "enter n: ";
     std::cin >> n;
-    std::cout << "enter m ";
-    std::cin >> m;
-    std::cout << "enter matrix values:" << std::endl;
+    std::cout << "enter matrix elements: " << std::endl;
+
     for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < n; ++j)
+        for (int j = 0; j < n; j++)
         {
-            std::cin >> mas[i][j];
+            std::cin >> matrix[i][j];
         }
     }
 }
@@ -23,7 +22,7 @@ bool isPrime(int num)
     {
         return false;
     }
-    for (int i=2; i*i<=num; i++)
+    for (int i = 2; i * i <= num; i++)
     {
         if (num % i == 0)
         {
@@ -33,14 +32,14 @@ bool isPrime(int num)
     return true;
 }
 
-bool checkColumn(int mas[N_Max][M_Max], int n, int m)
+bool checkColumn(int matrix[N_Max][N_Max], int n, int col)
 {
     int sum = 0;
     bool hasPrime = false;
 
-    for (int row = 0; row < n; ++row)
+    for (int row = 0; row < n; row++)
     {
-        int value = mas[row][m];
+        int value = matrix[row][col];
         sum += value;
         if (isPrime(value))
         {
@@ -51,11 +50,11 @@ bool checkColumn(int mas[N_Max][M_Max], int n, int m)
     return hasPrime = false && sum == 0;
 }
 
-void sortRow(int row[], int n)
+void sortRowByAbs(int row[], int n)
 {
-    for (int i = 0; i < n - 1; ++i)
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; ++j)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (abs(row[j]) > abs(row[j + 1]))
             {
@@ -65,33 +64,13 @@ void sortRow(int row[], int n)
     }
 }
 
-void swapRows(int a[], int b[], int m)
-{
-    for (int i = 0; i < m; i++)
-    {
-        int tmp = a[i];
-        a[i] = b[i];
-        b[i] = tmp;
-    }
-}
-
-int rowProduct(int row[], int m)
-{
-    int product = 1;
-    for(int i=0; i<m; i++)
-    {
-        product *= row[i];
-    }
-    return product;
-}
-
-void proccess(int mas[N_Max][M_Max], int n, int m)
+void processMatrix(int matrix[N_Max][N_Max], int n)
 {
     bool columnFound = false;
 
-    for (int j = 0; j < n; ++j)
+    for (int j = 0; j < n; j++)
     {
-        if (checkColumn(mas, n, j))
+        if (checkColumn(matrix, n, j))
         {
             columnFound = true;
             break;
@@ -100,25 +79,25 @@ void proccess(int mas[N_Max][M_Max], int n, int m)
 
     if (columnFound)
     {
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
         {
-            rowProduct(mas[i], n);
+            sortRowByAbs(matrix[i], n);
         }
     }
     else
     {
-        std::cout << "not found" << std::endl;
+        std::cout << "column not found" << std::endl;
     }
 }
 
-void Write(int mas[N_Max][M_Max], int n, int m)
+void writeMatrix(int matrix[N_Max][N_Max], int n)
 {
-    std::cout << "Output:" << std::endl;
-    for (int i = 0; i < n; ++i)
+    std::cout << "matrix after proccessing:" << std::endl;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; ++j)
+        for (int j = 0; j < n; j++)
         {
-            std::cout << mas[i][j] << " ";
+            std::cout << matrix[i][j] << " ";
         }
         std::cout << std::endl;
     }
